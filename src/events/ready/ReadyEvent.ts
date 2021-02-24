8a3f58050d43426f086e48bee7c02271c5e9ec09
Tizianogh <tiziano.ghisotti@gmail.com>
2021-02-24 20:51:10 +0100

    delete slappey

--- src/events/ready/ReadyEvent.ts
+++
@@ -1,9 +0,0 @@
-import BaseEvent from '../../utils/structures/BaseEvent';
-export default class ReadyEvent extends BaseEvent {
-    constructor() {
-        super(`ready`);
-    }
-    async run () {
-        console.log(`Bot has logged in.`);
-    }
-}
