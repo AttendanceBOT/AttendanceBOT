7aeedeb6a4dbdc5b4bdcc313702a67a7c523867a
Tiziano Ghisotti <tiziano.ghisotti@gmail.com>
2021-03-03 12:39:56 +0100

    fixes test

--- src/tests/ping.test.ts
+++ src/tests/ping.test.ts
@@ -1,12 +1,12 @@
 import "reflect-metadata";
 import 'mocha';
 import {expect} from 'chai';
-import {PingFinder} from "../commands/ping-finder"
+import {PingFinder} from "../../src/commands/ping-finder"
 import {EmbedRoll} from "../services/embed-roll";
 import {instance, mock, verify, when} from "ts-mockito";
 import {Message} from "discord.js";
 
-describe('MessageResponder', () => {
+describe('EmbedRoll', () => {
     let mockedPingFinderClass: PingFinder;
     let mockedPingFinderInstance: PingFinder;
     let mockedMessageClass: Message;
@@ -22,14 +22,6 @@ describe('MessageResponder', () => {
         setMessageContents();
 
         service = new EmbedRoll(mockedPingFinderInstance);
-    })
-
-    it('should reply', async () => {
-        whenIsPingThenReturn(true);
-
-        await service.handle(mockedMessageInstance);
-
-        verify(mockedMessageClass.reply('pong!')).once();
     })
 
     it('should not reply', async () => {
@@ -53,4 +45,3 @@ describe('MessageResponder', () => {
         when(mockedPingFinderClass.isTriggerCommand("Non-empty string")).thenReturn(result);
     }
 });
-
